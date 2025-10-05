1419644bf58a7215bf92d39e1563cc0dd4e56187
osamakanaan593 <osamakanaan593@gmail.com>
2025-10-05 04:02:17 +0000

    fix(build): handle async lessons in kit header

--- components/kit-header.tsx
+++ components/kit-header.tsx
@@ -1,30 +1,37 @@
 'use client'
 
-import { useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getKits, getLessons } from '@/lib/data'
-import { useParams } from 'next/navigation'
 import Link from 'next/link'
 import SearchPanel from './search-panel'
 import { i18n, t, Locale } from '@/lib/i18n'
+import { Lesson } from '@/lib/types'
 
 export default function KitHeader({ lang, kitSlug, lessonSlug }: { lang: Locale; kitSlug: string, lessonSlug: string }) {
   const [searchQuery, setSearchQuery] = useState('')
+  const [lessonsForWiki, setLessonsForWiki] = useState<(Lesson & { kitSlug: string })[]>([]);
 
-  const lessonsForWiki = useMemo(() => {
-    const allKits = getKits();
-    // Find the current kit to identify its wiki
-    const currentKit = allKits.find(k => k.slug === kitSlug);
-    const currentWikiSlug = currentKit?.wikiSlug;
+  useEffect(() => {
+    const fetchLessonsForWiki = async () => {
+      const allKits = getKits();
+      const currentKit = allKits.find(k => k.slug === kitSlug);
+      const currentWikiSlug = currentKit?.wikiSlug;
 
-    if (currentWikiSlug) {
-      // Get all kits belonging to the same wiki
-      const kitsInSameWiki = allKits.filter(k => k.wikiSlug === currentWikiSlug);
-      // Get all lessons from all kits in the same wiki
-      return kitsInSameWiki.flatMap(k => getLessons(k.slug).map(l => ({ ...l, kitSlug: k.slug })));
-    } else {
-      // Fallback for kits not associated with a wiki
-      return getLessons(kitSlug).map(l => ({ ...l, kitSlug }));
-    }
+      if (currentWikiSlug) {
+        const kitsInSameWiki = allKits.filter(k => k.wikiSlug === currentWikiSlug);
+        const lessonPromises = kitsInSameWiki.map(async k => {
+          const lessons = await getLessons(k.slug);
+          return lessons.map(l => ({ ...l, kitSlug: k.slug }));
+        });
+        const lessonsFromAllKits = await Promise.all(lessonPromises);
+        setLessonsForWiki(lessonsFromAllKits.flat());
+      } else {
+        const lessons = await getLessons(kitSlug);
+        setLessonsForWiki(lessons.map(l => ({ ...l, kitSlug })));
+      }
+    };
+
+    fetchLessonsForWiki();
   }, [kitSlug]);
 
   const otherLocale = i18n.locales.find((l) => l !== lang);

18529b10ec8bbea232b6e0600b805e3c52db5c1e
Osama Kanan <osamakanaan593@gmail.com>
2025-11-03 12:51:25 +0200

    build: externalize ssh2 dependencies

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   experimental: {
     outputFileTracingRoot: undefined,
+    serverComponentsExternalPackages: ['ssh2', 'ssh2-sftp-client'],
   },
   // Disable source maps in production for smaller builds
   productionBrowserSourceMaps: false,
@@ -9,6 +10,14 @@ const nextConfig = {
   trailingSlash: false,
   // Disable x-powered-by header
   poweredByHeader: false,
+  webpack: (config) => {
+    config.externals = config.externals || []
+    config.externals.push({
+      'ssh2-sftp-client': 'commonjs ssh2-sftp-client',
+      ssh2: 'commonjs ssh2',
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig

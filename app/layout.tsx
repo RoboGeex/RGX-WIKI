3ea87809c378cad8d591fe0d5d7f18938b5e215d
osama <info@robogeex.com>
2025-11-24 13:34:58 +0200

    Set favicon to robogeex wordmark

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'RGX Kits Wiki',
   description: 'Arduino lesson wiki',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {

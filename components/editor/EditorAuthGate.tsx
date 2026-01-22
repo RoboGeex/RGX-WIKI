0306ca025ce616c73801f21afe735d672f9eb2f8
osama <info@robogeex.com>
2026-01-22 12:25:58 +0200

    Fix editor auth hydration

--- components/editor/EditorAuthGate.tsx
+++ components/editor/EditorAuthGate.tsx
@@ -5,13 +5,23 @@ import DeveloperLogin from "./DeveloperLogin"
 import { getDeveloperId, rememberDeveloperId } from "./dev-identity"
 
 export default function EditorAuthGate({ children }: { children: React.ReactNode }) {
-  const [devId, setDevId] = useState<string | undefined>(() => getDeveloperId())
+  const [devId, setDevId] = useState<string | undefined>(undefined)
+  const [hydrated, setHydrated] = useState(false)
+
+  useEffect(() => {
+    setHydrated(true)
+    setDevId(getDeveloperId())
+  }, [])
 
   useEffect(() => {
     if (devId) {
       rememberDeveloperId(devId)
     }
   }, [devId])
+
+  if (!hydrated) {
+    return null
+  }
 
   if (!devId) {
     return <DeveloperLogin onSignedIn={setDevId} />
